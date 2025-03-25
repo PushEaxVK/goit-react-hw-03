ae7af4dc1095557400d9041158b5b3aac72ccbfd
PushEax <pusheax.vk@gmail.com>
2025-03-26 00:57:45 +0200

    ContactForm structure of component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,56 @@
 import css from './ContactForm.module.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useId } from 'react';
+import * as Yup from 'yup';
+
+const ContactSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  number: Yup.string()
+    .min(3, 'Too Short!')
+    .max(256, 'Too Long!')
+    .required('Required'),
+});
+
+const initialValues = {
+  name: '',
+  number: '',
+};
 
 const ContactForm = () => {
-  return <div></div>;
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={ContactSchema}
+    >
+      <Form>
+        <div>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field type="text" name="name" id={nameFieldId} />
+
+          <ErrorMessage name="name" component="span" />
+        </div>
+        <div>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field type="text" name="number" id={numberFieldId} />
+
+          <ErrorMessage name="number" component="span" />
+        </div>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;

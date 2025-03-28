26d61a0b7ced67a4dd432fb442b3ef29084a2eb3
PushEax <pusheax.vk@gmail.com>
2025-03-28 04:32:31 +0200

    complete ContactList component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,15 @@
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 
-const ContactList = ({ list }) => {
+const ContactList = ({ list, handleRemoveContact }) => {
   return (
-    <ul>
+    <ul className={css.list}>
       {list.map((contact) => (
         <li key={contact.id}>
-          <Contact contact={contact} />
+          <Contact
+            contact={contact}
+            handleRemoveContact={handleRemoveContact}
+          />
         </li>
       ))}
     </ul>

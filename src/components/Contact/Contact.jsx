dc953deec6e29681e1589bd90280d5505fb14bdc
PushEax <pusheax.vk@gmail.com>
2025-03-28 04:31:24 +0200

    complete Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,27 @@
 import css from './Contact.module.css';
+import { FaUser } from 'react-icons/fa6';
+import { FaPhone } from 'react-icons/fa6';
 
-const Contact = ({ contact }) => {
+const Contact = ({ contact, handleRemoveContact }) => {
   return (
-    <div>
-      <p>{contact.name}</p>
-      <p>{contact.number}</p>
-      <button>Delete</button>
+    <div className={css.contact}>
+      <div className={css.text}>
+        <p>
+          <FaUser className={css.icon} />
+          {contact.name}
+        </p>
+        <p>
+          <FaPhone className={css.icon} />
+          {contact.number}
+        </p>
+      </div>
+      <button
+        type="button"
+        className={css.button}
+        onClick={() => handleRemoveContact(contact.id)}
+      >
+        Delete
+      </button>
     </div>
   );
 };

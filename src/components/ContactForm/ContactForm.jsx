add5e3330addbab44864a2b88fa1e631d0c67558
PushEax <pusheax.vk@gmail.com>
2025-03-28 04:32:09 +0200

    complete ContactForm component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,6 +2,7 @@ import css from './ContactForm.module.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useId } from 'react';
 import * as Yup from 'yup';
+import { nanoid } from 'nanoid';
 
 const ContactSchema = Yup.object().shape({
   name: Yup.string()
@@ -19,12 +20,15 @@ const initialValues = {
   number: '',
 };
 
-const ContactForm = () => {
+const ContactForm = ({ handleAddContact }) => {
   const nameFieldId = useId();
   const numberFieldId = useId();
 
   const handleSubmit = (values, actions) => {
-    console.log(values);
+    handleAddContact({
+      ...values,
+      id: nanoid(),
+    });
     actions.resetForm();
   };
 
@@ -34,20 +38,22 @@ const ContactForm = () => {
       onSubmit={handleSubmit}
       validationSchema={ContactSchema}
     >
-      <Form>
-        <div>
+      <Form className={css.contactForm}>
+        <div className={css.fieldBox}>
           <label htmlFor={nameFieldId}>Name</label>
           <Field type="text" name="name" id={nameFieldId} />
 
           <ErrorMessage name="name" component="span" />
         </div>
-        <div>
+        <div className={css.fieldBox}>
           <label htmlFor={numberFieldId}>Number</label>
           <Field type="text" name="number" id={numberFieldId} />
 
           <ErrorMessage name="number" component="span" />
         </div>
-        <button type="submit">Add contact</button>
+        <button type="submit" className={css.sendButton}>
+          Add contact
+        </button>
       </Form>
     </Formik>
   );
